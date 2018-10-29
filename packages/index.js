0ab42a8b95effbe4ccadd9e5d1e7758752af9155
jiang xiaosong <xiaosong402@zuozh.com>
2018-10-29 21:30:30 +0800

    WIP

---
+++ packages/index.js
@@ -0,0 +1,19 @@
+import GChart from './g-chart'
+
+const components = [
+  GChart
+]
+
+const install = function (Vue) {
+  if (install.installed) return
+  components.map(component => Vue.component(component.name, component))
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  GChart
+}
