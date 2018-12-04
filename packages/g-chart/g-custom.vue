769e76b9b47a0856b60671e8f68bd869751e549e
xiaosong.jiang <xiaosong.jiang@geely.com>
2018-12-04 16:52:40 +0800

    add g-custom 添加自定义图表

---
+++ packages/g-chart/g-custom.vue
@@ -0,0 +1,20 @@
+<script>
+	import GBase from './base/g-base'
+	export default {
+		extends: GBase,
+		name: "g-custom",
+		props: ['data', 'name', 'itemStyle', 'renderItem'],
+		data(){
+			return {
+				options:{
+					type: 'custom',
+					...this.$props,
+				}
+			}
+		},
+		created(){
+			if(!this.chartsOptions.series) this.chartsOptions.series = []
+			this.chartsOptions.series.push(this.options)
+		},
+	}
+</script>
