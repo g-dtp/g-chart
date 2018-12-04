2cc4ce8dd400fe7179aafd5dea679423991a836e
xiaosong.jiang <xiaosong.jiang@geely.com>
2018-12-04 13:57:17 +0800

    update g-area

---
+++ packages/g-chart/g-area.vue
@@ -0,0 +1,20 @@
+<script>
+	import GBase from './base/g-base'
+	export default {
+		extends: GBase,
+		name: "g-line",
+		props: ['data', 'name', 'smooth', 'showSymbol'],
+		data() {
+			return {
+				options: {
+					type: 'line',
+					...this.$props
+				}
+			}
+		},
+		created() {
+			if (!this.chartsOptions.series) this.chartsOptions.series = []
+			this.chartsOptions.series.push(this.options)
+		}
+	}
+</script>
