30f7c4e4f17ca4fa17a795696e5ccb13fa80ec80
xiaosong.jiang <xiaosong.jiang@geely.com>
2018-12-06 11:27:58 +0800

    WIP

--- packages/utils/chart-helper.js
+++ packages/utils/chart-helper.js
@@ -1,9 +1,11 @@
 import echarts from 'echarts'
 
 /*
-* 用来覆盖默认
+* 用来覆盖默认主题
 * */
-export function registerTheme(name, theme) {
-	theme = theme || {}
-	echarts.registerTheme(name, theme)
+export default {
+	registerTheme: function (name, theme) {
+		theme = theme || {}
+		echarts.registerTheme(name, theme)
+	}
 }
