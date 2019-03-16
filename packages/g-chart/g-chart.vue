0fb39bb7e7933c6c348be6f7d2086ca3d4aed2a1
GEELY\LiNa.Zhang <2481006364qq.com>
2019-03-16 10:28:18 +0800

    取消代码

--- packages/g-chart/g-chart.vue
+++ packages/g-chart/g-chart.vue
@@ -65,9 +65,6 @@
 				vm.timer = setTimeout(() => {
 					if (vm.chart) vm.chart.resize()
 				}, 0)
-				window.addEventListener("resize", () => { 
-				    vm.chart.resize()
-				})
 			},
 			onChartClick (series) {
 				if (series.seriesType) return
